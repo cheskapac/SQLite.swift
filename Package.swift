735ab8094a06eae4492d85c09893f408f4bc9ddf
Paulius Cesekas <paulius.cesekas@tesonet.com>
2020-09-01 10:14:14 +0300

    Update swift package details

--- Package.swift
+++ Package.swift
@@ -4,27 +4,23 @@ import PackageDescription
 let package = Package(
     name: "SQLite.swift",
     products: [
-        .library(
-            name: "SQLite",
-            targets: ["SQLite"]
-        )
+        .library(name: "SQLite", targets: ["SQLite"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/antwork/SQLCipher.git",
-            from: "0.0.4"
-        )
+        .package(url: "https://github.com/antwork/SQLCipher.git", from: "0.0.4")
     ],
     targets: [
         .target(
             name: "SQLite",
-            dependencies: ["SQLiteObjc", "SQLCipher"],
+            dependencies: [
+                "SQLiteObjc",
+                .product(name: "SQLCipher", package: "SQLCipher"),
+            ],
             swiftSettings: [
                 .define("SQLITE_SWIFT_SQLCIPHER")
             ]
         ),
         .target(name: "SQLiteObjc"),
-        .target(name: "SQLCipher"),
         .testTarget(
             name: "SQLiteTests",
             dependencies: ["SQLite"],
